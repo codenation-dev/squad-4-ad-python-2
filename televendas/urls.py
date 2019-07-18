b34f9c74cf250bbfe595f77fb72ef9e81c17981c
luanfsf <luanfsf@gmail.com>
2019-07-18 14:58:58 -0300

    Substituir url por path nas urls do projeto televendas

--- televendas/urls.py
+++ televendas/urls.py
@@ -1,9 +1,8 @@
-from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    url(r'^api-auth/', include('rest_framework.urls')),
+    path('api-auth/', include('rest_framework.urls')),
 ]
