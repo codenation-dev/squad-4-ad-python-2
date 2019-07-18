c8584dee1b536c80baaf162e3f78fbefac561ac0
luanfsf <luanfsf@gmail.com>
2019-07-18 10:24:47 -0300

    Adicionar drf-ysag, markdown, pyyaml para documentação Adicionar psycopg2-binary para utilizar postgres Adicionar django-filter para filtrar querysets

--- televendas/settings.py
+++ televendas/settings.py
@@ -35,8 +35,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
     'api',
+    'rest_framework',
+    'django_filters',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
