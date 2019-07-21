f8dab8d7b1bd5eb76f8cb793a24c3b484bbd60ac
Julio Cezar Riffel <julioriffel@gmail.com>
2019-07-20 23:37:05 -0300

    GET Lista de vendedores ordenados pelo valor da comissão

--- api/views/month_comission.py
+++ api/views/month_comission.py
@@ -1,18 +1,21 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from api.models import Sale, Seller
 
-@api_view(["POST"])
-def month_comission_view(request):
-    """
-    TODO
 
-    Criar uma venda para o vendedor
-    De acordo com a descrição da view /month_comission em description.py
+@api_view(["GET"])
+def month_comission_view(request, pk):
+    print(pk)
 
-    """
-    data = request.data
+    r = []
+    sellers = Seller.objects.all()
 
-    serializer = "Utilizar um serializer para registrar a respectiva venda"
+    for sell in sellers:
+        try:
+            sale = Sale.objects.filter(month=pk, seller=sell.pk)[0]
+            r.append({"id": sell.pk, "nome": sell.name, "comission": sale.comission_value})
+        except:
+            r.append({"id": sell.pk, "nome": sell.name, "comission": 0.0})
 
-    return Response("created")
+    return Response(sorted(r, key=lambda i: i['comission'], reverse=True))
