d858acf8ddfa391d27d521a6f4a7f1badf5d0309
luanfsf <luanfsf@gmail.com>
2019-07-22 04:04:06 -0300

    Ajustes no teste test_send_email Adicionar banco para testes em settings.py Adicionar teste da função de notificação

--- api/tests/test_send_email.py
+++ api/tests/test_send_email.py
@@ -1,28 +1,30 @@
-from api.tests.base_api_test import BaseApiTest
 
 from django.core import mail
+from rest_framework.test import APITestCase
+
 from api.models import Seller, Plan
+from api.tests.base_api_test import BaseApiTest
 
 from api.views.check_comission import send_email_sale_notification
 
 
-class TestMail(BaseApiTest):
+class TestMail(BaseApiTest, APITestCase):
     """
     Tests if email is being send
     """
-    seller = Seller.objects.create(
-        name="test",
-        address="teste",
-        phone="47",
-        birthday='2000-01-01',
-        email="fake@mail.com",
-        cpf="12345678901",
-        plan=Plan.objects.all().first()
-    )
 
     def test_no_email_sent(self):
         self.assertEqual(len(mail.outbox), 0)
 
     def test_email_sent(self):
-        send_email_sale_notification(seller=self.seller)
+        seller = Seller.objects.create(
+            name="test",
+            address="teste",
+            phone="47",
+            birthday='2000-01-01',
+            email="fake@mail.com",
+            cpf="12345678901",
+            plan=Plan.objects.all().first()
+        )
+        send_email_sale_notification(seller=seller)
         self.assertEqual(len(mail.outbox), 1)
---
+++ api/tests/test_should_notify.py
@@ -0,0 +1,49 @@
+from api.tests.base_api_test import BaseApiTest
+
+from api.models import Seller, Plan, Sale
+
+from api.views.check_comission import should_notify_user
+
+
+class TestShouldNotify(BaseApiTest):
+    """
+    Tests if email is being send
+    """
+
+    def generate_sales(self, seller, year, amount):
+        for month in range(1, 6):
+            Sale.objects.create(
+                seller=seller,
+                amount=amount,
+                year=year,
+                month=month
+            )
+
+    def get_seller(self):
+        return Seller.objects.create(
+            name="test",
+            address="teste",
+            phone="47",
+            birthday='2000-01-01',
+            email="fake@mail.com",
+            cpf="12345678901",
+            plan=Plan.objects.all().first()
+        )
+
+    def test_should_not_notify(self):
+        AMOUNT = 500
+        seller = self.get_seller()
+
+        self.generate_sales(seller=seller, year=2018, amount=AMOUNT)
+
+        self.assertFalse(should_notify_user(seller=seller, amount=AMOUNT))
+
+    def test_should_notify(self):
+        AMOUNT = 1000
+        LOWER_AMOUNT = 100
+
+        seller = self.get_seller()
+
+        self.generate_sales(seller=seller, year=2019, amount=AMOUNT)
+
+        self.assertTrue(should_notify_user(seller=seller, amount=LOWER_AMOUNT))
