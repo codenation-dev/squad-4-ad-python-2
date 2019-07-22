fe31cfe66f1a85f3b6cb9c8aaea41d76ef774b49
luanfsf <luanfsf@gmail.com>
2019-07-22 00:03:40 -0300

    Adicionar teste para verificar envio de emails

---
+++ api/tests/test_send_email.py
@@ -0,0 +1,28 @@
+from api.tests.base_api_test import BaseApiTest
+
+from django.core import mail
+from api.models import Seller, Plan
+
+from api.views.check_comission import send_email_sale_notification
+
+
+class TestMail(BaseApiTest):
+    """
+    Tests if email is being send
+    """
+    seller = Seller.objects.create(
+        name="test",
+        address="teste",
+        phone="47",
+        birthday='2000-01-01',
+        email="fake@mail.com",
+        cpf="12345678901",
+        plan=Plan.objects.all().first()
+    )
+
+    def test_no_email_sent(self):
+        self.assertEqual(len(mail.outbox), 0)
+
+    def test_email_sent(self):
+        send_email_sale_notification(seller=self.seller)
+        self.assertEqual(len(mail.outbox), 1)
