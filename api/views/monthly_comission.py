b64354627005f5cc22427d171d913672d0849f96
Julio Cezar Riffel <julioriffel@gmail.com>
2019-07-22 23:18:23 -0300

    CORREÇAO Argumento com nome não relacionado, path com name diferente do path fix #36

--- api/views/monthly_comission.py
+++ api/views/monthly_comission.py
@@ -5,15 +5,13 @@ from api.models import Sale, Seller
 
 
 @api_view(["GET"])
-def monthly_comission_view(request, pk):
-    print(pk)
-
+def monthly_comission_view(request, month):
     r = []
     sellers = Seller.objects.all()
 
     for sell in sellers:
         try:
-            sale = Sale.objects.filter(month=pk, seller=sell.pk)[0]
+            sale = Sale.objects.filter(month=month, seller=sell.pk)[0]
             r.append({"id": sell.pk, "nome": sell.name, "comission": sale.comission_value})
         except:
             r.append({"id": sell.pk, "nome": sell.name, "comission": 0.0})
