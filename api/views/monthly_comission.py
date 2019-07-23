3abfddaef4f23cc69e97c59f3bc71b0b468f12c2
luanfsf <luanfsf@gmail.com>
2019-07-23 03:37:08 -0300

    Adicionar teste para view monthly_comission_view Ajustes na view monthly_comission_view

--- api/views/monthly_comission.py
+++ api/views/monthly_comission.py
@@ -1,19 +1,20 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
+from datetime import datetime
 from api.models import Sale, Seller
 
 
 @api_view(["GET"])
 def monthly_comission_view(request, month):
+    CURRENT_YEAR = datetime.now().year
     r = []
     sellers = Seller.objects.all()
 
-    for sell in sellers:
+    for sale in sellers:
         try:
-            sale = Sale.objects.filter(month=month, seller=sell.pk)[0]
-            r.append({"id": sell.pk, "nome": sell.name, "comission": sale.comission_value})
-        except:
-            r.append({"id": sell.pk, "nome": sell.name, "comission": 0.0})
+            sale = Sale.objects.get(year=CURRENT_YEAR, month=month, seller=sale.pk)
+            r.append({"id": sale.pk, "name": sale.seller.name, "comission": sale.comission_value})
+        except (Sale.DoesNotExist, Sale.MultipleObjectsReturned) as e:
+            print(f"{e}")
 
     return Response(sorted(r, key=lambda i: i['comission'], reverse=True))
