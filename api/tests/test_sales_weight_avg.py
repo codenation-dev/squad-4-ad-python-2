2c9ecde3216b76857c08ef9ce5e3c3996dd869e4
luanfsf <luanfsf@gmail.com>
2019-07-23 02:18:17 -0300

    Adicionar teste unitário da função sales_weight_avg

---
+++ api/tests/test_sales_weight_avg.py
@@ -0,0 +1,46 @@
+from api.models import Seller, Plan, Sale
+from api.tests.base_api_test import BaseApiTest
+
+from api.views.check_comission import sales_weight_avg, get_seller_last_sales
+
+
+class TestSalesWeightAvg(BaseApiTest):
+    """
+    Tests if weighted average is correct
+    """
+    def generate_const_sales(self, seller, year, amount):
+        for month in range(1, 6):
+            Sale.objects.create(
+                seller=seller,
+                amount=amount,
+                year=year,
+                month=month
+            )
+
+    def get_seller(self):
+        return Seller.objects.create(
+            name="test",
+            address="teste",
+            phone="47",
+            birthday='2000-01-01',
+            email="fake@mail.com",
+            cpf="12345678901",
+            plan=Plan.objects.all().first()
+        )
+
+    def test_const_sales_weight_avg(self):
+        CONSTANT_AMOUNT = 500
+        MAX_LAST_SALES = 5
+
+        seller = self.get_seller()
+
+        self.generate_const_sales(seller=seller, year=2019, amount=CONSTANT_AMOUNT)
+
+        last_sales = get_seller_last_sales(seller=seller, max_last_sales=MAX_LAST_SALES)
+
+        sale_weighted_avg = sales_weight_avg(sales=last_sales)
+
+        self.assertEquals(
+            sale_weighted_avg,
+            CONSTANT_AMOUNT
+        )
