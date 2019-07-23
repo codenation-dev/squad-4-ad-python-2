1b5f8f0e2e0428f48179fd5d7f9ff57b729c4a00
Julio Cezar Riffel <julioriffel@gmail.com>
2019-07-23 20:56:24 -0300

    Add Documentação View plans e plans/pk/ #48

--- api/views/plan.py
+++ api/views/plan.py
@@ -5,6 +5,11 @@ from api.serializer.plan import PlanSerializer
 
 
 class PLanListView(generics.ListCreateAPIView):
+    """ Cadastro dos planos de comissões para que os vendedores possam escolher qual o melhor plano para eles.
+Cada plano possui dum nível básico de remuneração e outro nível maior quando a meta de vendas é cumprida
+
+GET retorna a lista de todos os planos
+POST inclusão de novo plano"""
 
     serializer_class = PlanSerializer
 
@@ -13,6 +18,15 @@ class PLanListView(generics.ListCreateAPIView):
 
 
 class PlanView(generics.RetrieveUpdateDestroyAPIView):
+    """ Cadastro dos planos de comissões para que os vendedores possam escolher qual o melhor plano para eles.
+Cada plano possui dum nível básico de remuneração e outro nível maior quando a meta de vendas é cumprida
+
+{id}: Código do plano
+
+GET Retorna os dados do plano {id}
+PUT Atualização dos dados do plano {id}
+PATCH Atualização parcial dos dados do plano {id}
+DELETE Exclusão do registro"""
 
     lookup_field = 'pk'
     serializer_class = PlanSerializer
